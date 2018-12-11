1df6cbd5e50969cf3ae41aef04ac3c472ead3cbf
beerfekt <christoph.beer@gmx.de>
2018-12-11 16:37:35 +0100

    "added show_hide exercise"

---
+++ hide_show/js/custom.js
@@ -0,0 +1,17 @@
+
+
+
+var employees = [
+    { name: "Ben", gender: "Male", city: "London", salary: 55000 },
+    { name: "Sara", gender: "Female", city: "Chennai", salary: 68000 },
+    { name: "Mark", gender: "Male", city: "Chicago", salary: 57000 },
+    { name: "Pam", gender: "Female", city: "London", salary: 53000 },
+    { name: "Todd", gender: "Male", city: "Chennai", salary: 60000 }
+];
+
+
+var meineApp = angular
+    .module('meineApp', [])
+    .controller('meinController', function($scope){
+        $scope.employees = employees;
+    });
