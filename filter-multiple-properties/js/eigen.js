bbfffb274a41a8644ad0acd11e8750f86e6aa316
beerfekt <christoph.beer@gmx.de>
2018-12-11 15:15:46 +0100

    "finished search with properties second form"

--- filter-multiple-properties/js/eigen.js
+++ filter-multiple-properties/js/eigen.js
@@ -22,10 +22,37 @@ var employees = [
 
 
 
+
 var meineApp = angular.module('meineApp', [])
     .controller('meinController', function($scope){
         $scope.employees = employees;
+    })
+    .controller('einController', function($scope){
+        $scope.employees = employees;
+
+        //For every item ...
+        $scope.sucheEigen = function (item) {
+
+            if( $scope.searchText == undefined){
+                //if we have nothing in textbox
+                // display all elements (true foreach)
+                return true;
+            } else {
+                //if we have something in the textbox
+                if ( item.city.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 ||
+                    item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1
+                    ) {
+                    //display current element
+                    return true;
+                }
+            }
+            //if nothing matches, return false and do not display the current item
+            return false;
+
+        }
     });
+
+//2 Controller um gleichen Datensatz unabhängig zu bearbeiten, ohne dass alle Felder synchronisieren
 
 
 
