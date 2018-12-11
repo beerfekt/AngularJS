84c8ebfff4d18ed90b697e66deaff6a6645cd505
beerfekt <christoph.beer@gmx.de>
2018-12-11 15:57:16 +0100

    "moved filter to filters.js"

--- custom-filter/js/eigen.js
+++ custom-filter/js/eigen.js
@@ -26,21 +26,6 @@ var employees = [
 
 var meineApp = angular
     .module('meineApp', [])
-    .filter( 'gender', function() {
-        return function (gender){
-            switch (gender) {
-                case 1 :
-                    return 'male';
-                case 2 :
-                    return 'female';
-                case 3 :
-                    return 'unknown';
-            }
-        }
-    })
     .controller('meinController', function($scope){
         $scope.employees = employees;
-
-
-
     });
